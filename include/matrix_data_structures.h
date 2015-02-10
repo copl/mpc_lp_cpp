180d13733654133238cd238f84ebf600816bf82c
Santiago Akle <tiagoakle@gmail.com>
2015-02-09 18:38:07 -0800

    Moved the code from mpc_lp_julia

---
+++ include/matrix_data_structures.h
@@ -0,0 +1,21 @@
+#ifndef MATRIX_POINT_DATA_STRUCTURES
+#define MATRIX_DATA_STRUCTURES
+
+class class_matrix {
+};
+
+class class_vector {
+	//	this.zeros() = 0
+	//
+	//	this.multiply(A,x) = Ax
+	//
+	//	this.add(x,y) = x + y
+	//
+	//	this.multiply_add(A,x) = this + Ax 
+	//
+	//	this.subtract(x,y) = x - y
+	//
+	//	this.multiply_subtract(A,x) = this - Ax
+};
+
+#endif
