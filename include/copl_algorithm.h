4da928d0de1485826424788c9642c56428b9fa3e
Oliver Hinder <ohinder@gmail.com>
2015-02-13 11:00:19 -0800

    st

---
+++ include/copl_algorithm.h
@@ -0,0 +1,9 @@
+#ifndef INTERIOR_POINT_ALGORITHM
+#define INTERIOR_POINT_ALGORITHM
+
+#include "copl_core.h"
+
+void interior_point_algorithm(copl_ip::lp_input problem_data, copl_ip::settings settings);
+
+
+#endif
